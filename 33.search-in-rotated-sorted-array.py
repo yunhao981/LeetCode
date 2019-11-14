5e646b2a83f95ec14f732d64b3a44317e75e5e55
Yunhao Liu <yunhao981@hotmail.com>
2019-11-15 04:11:39 +0800

    day10

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (33.09%)
+# Likes:    3224
+# Dislikes: 373
+# Total Accepted:    515.4K
+# Total Submissions: 1.5M
+# Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+#
+# Suppose an array sorted in ascending order is rotated at some pivot unknown
+# to you beforehand.
+# 
+# (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+# 
+# You are given a target value to search. If found in the array return its
+# index, otherwise return -1.
+# 
+# You may assume no duplicate exists in the array.
+# 
+# Your algorithm's runtime complexity must be in the order of O(log n).
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# 
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        low, high = 0, len(nums) - 1
+        if target == nums[low]:
+            return low
+        elif target == nums[high]:
+            return high
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+# @lc code=end

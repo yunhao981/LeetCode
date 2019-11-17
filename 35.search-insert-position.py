105640813fe210a40c0852b27d2228d978b5f3fa
Yunhao Liu <yunhao981@hotmail.com>
2019-11-17 21:18:28 +0800

    day12

---
+++ 35.search-insert-position.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+# https://leetcode.com/problems/search-insert-position/description/
+#
+# algorithms
+# Easy (41.09%)
+# Likes:    1627
+# Dislikes: 207
+# Total Accepted:    479.7K
+# Total Submissions: 1.2M
+# Testcase Example:  '[1,3,5,6]\n5'
+#
+# Given a sorted array and a target value, return the index if the target is
+# found. If not, return the index where it would be if it were inserted in
+# order.
+# 
+# You may assume no duplicates in the array.
+# 
+# Example 1:
+# 
+# 
+# Input: [1,3,5,6], 5
+# Output: 2
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [1,3,5,6], 2
+# Output: 1
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: [1,3,5,6], 7
+# Output: 4
+# 
+# 
+# Example 4:
+# 
+# 
+# Input: [1,3,5,6], 0
+# Output: 0
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
+
+        
+
+# @lc code=end
+

f47c848d1c10bc85120358e3ef48f8a59e7aaecc
Yunhao Liu <yunhao981@hotmail.com>
2019-11-19 23:53:05 +0800

    day14

--- 43.multiply-strings.py
+++ 43.multiply-strings.py
@@ -46,5 +46,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         # return str(int(num1) * int(num2))
+        res = [0] * (len(num1) + len(num2))
+        for i in range(len(num1)-1, -1, -1):
+            carry = 0
+            for j in range(len(num2)-1, -1, -1):
+                tmp = int(num1[i]) * int(num2[j]) + carry
+                carry = (res[i+j+1] + tmp) // 10
+                res[i+j+1] = (res[i+j+1] + tmp) % 10
+            res[i] += carry
+        res = "".join(map(str,res))
+        return '0' if not res.lstrip("0") else res.lstrip("0")
+
 # @lc code=end
 

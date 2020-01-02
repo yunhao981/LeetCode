75fe09e6fa221a3836f8ec881f13a375cbe11ee6
Yunhao Liu <yunhao981@hotmail.com>
2020-01-02 18:34:42 +0800

    Change to Leetcode-cn due to login failure

---
+++ 1.两数之和.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+# https://leetcode-cn.com/problems/two-sum/description/
+#
+# algorithms
+# Easy (47.21%)
+# Likes:    7292
+# Dislikes: 0
+# Total Accepted:    754K
+# Total Submissions: 1.6M
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+# 
+# 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+# 
+# 示例:
+# 
+# 给定 nums = [2, 7, 11, 15], target = 9
+# 
+# 因为 nums[0] + nums[1] = 2 + 7 = 9
+# 所以返回 [0, 1]
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] not in m:
+                m[target - nums[i]] = i
+            else:
+                return m[nums[i]], i
+        return -1, -1
+# @lc code=end
+
---
+++ 2.两数相加.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=2 lang=python3
+#
+# [2] 两数相加
+#
+# https://leetcode-cn.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (36.26%)
+# Likes:    3651
+# Dislikes: 0
+# Total Accepted:    289.9K
+# Total Submissions: 799.3K
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+# 
+# 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+# 
+# 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+# 
+# 示例：
+# 
+# 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+# 输出：7 -> 0 -> 8
+# 原因：342 + 465 = 807
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        root = ListNode(0)
+        n = root
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1 + v2 + carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+# @lc code=end
+

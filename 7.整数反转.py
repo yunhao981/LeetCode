061e6140bc7379ef650e10d4cb0c6bd19225cffe
Yunhao Liu <yunhao981@hotmail.com>
2020-01-08 23:37:06 +0800

    day21

---
+++ 7.整数反转.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode.cn id=7 lang=python3
+#
+# [7] 整数反转
+#
+# https://leetcode-cn.com/problems/reverse-integer/description/
+#
+# algorithms
+# Easy (33.38%)
+# Likes:    1582
+# Dislikes: 0
+# Total Accepted:    251.2K
+# Total Submissions: 752.2K
+# Testcase Example:  '123'
+#
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+# 
+# 示例 1:
+# 
+# 输入: 123
+# 输出: 321
+# 
+# 
+# 示例 2:
+# 
+# 输入: -123
+# 输出: -321
+# 
+# 
+# 示例 3:
+# 
+# 输入: 120
+# 输出: 21
+# 
+# 
+# 注意:
+# 
+# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+# 
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        s = str(x)[::-1].rstrip('-')
+        if int(s) < 2 ** 31:
+            if x > 0:
+                return int(s)
+            else:
+                return 0-int(s)
+        return 0
+# @lc code=end
+
